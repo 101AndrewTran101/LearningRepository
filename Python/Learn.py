fe8476a28c259014583beb05ca4e9ec137571421
Andy <101AndrewTran101@gmail.com>
2022-06-17 03:58:47 -0700

    Adding Python DAQmx

--- Python/Learn.py
+++ Python/Learn.py
@@ -1,5 +1,4 @@
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-    if x == "banana":
-        break
-    print(x)
+import nidaqmx
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("SimDev1/ai0")
+    print(task.read())
