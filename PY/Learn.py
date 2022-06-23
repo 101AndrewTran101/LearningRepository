649f599cd90a118fb57f7ebf23e4d86beb3e6b52
Andrew <101AndyBear101@gmail.com>
2022-06-23 02:57:06 -0700

    Learing For Loop

--- PY/Learn.py
+++ PY/Learn.py
@@ -1,4 +1,6 @@
-import nidaqmx
-with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("SimDev1/ai0")
-    print(task.read())
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+
