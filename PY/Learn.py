de52cce082d61e642d5db9cdfbb76f3e86c4b15f
Andrew <101AndyBear101@gmail.com>
2022-06-23 02:54:04 -0700

    Adding Learning Python

--- PY/Learn.py
+++ PY/Learn.py
@@ -1,20 +1,4 @@
-<<<<<<< HEAD:PY/Learn.py
-from turtle import pu
-
-
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-    if x == "banana":
-        break
-    print(x)
-
-pulledFruits = ["green", "orange", "red", "blue"]
-for y in pulledFruits:
-    print(y)
-    
-=======
 import nidaqmx
 with nidaqmx.Task() as task:
     task.ai_channels.add_ai_voltage_chan("SimDev1/ai0")
     print(task.read())
->>>>>>> master:Python/Learn.py
